c122c08a75cbd5585b0e78efd073a99a565b6d85
SudheerIJ <asudheer.info@gmail.com>
2024-03-18 10:17:28 +0530

    comit18mar

--- src/main/java/com/tutorialsninja/qa/utils/Utilities.java
+++ src/main/java/com/tutorialsninja/qa/utils/Utilities.java
@@ -17,14 +17,14 @@ import org.openqa.selenium.io.FileHandler;
 
 public class Utilities {
 	
-	public static final int IMPLICIT_WAIT_TIME=10;
-	public static final int PAGE_LOAD_TIME=5;
+	public static final int IMPLICIT_WAIT_TIME=30;
+	public static final int PAGE_LOAD_TIME=30;
 	
 	public static String generateEmailWithTimeStamp() {
 		
 		Date date = new Date();
 		String timestamp = date.toString().replace(" ","_").replace(":","_");
-		return "amotoori"+timestamp+"@gmail.com";
+		return "sudheer"+timestamp+"@gmail.com";
 		
 	}
 	
